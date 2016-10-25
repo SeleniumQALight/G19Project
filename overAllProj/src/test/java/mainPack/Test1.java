edeb9d03a79171bca4e01e5a220cd75ee74b5228
Andrey <angol1979@bigmir.net>
2016-10-25 22:00:19 +0300

    [QA] was added123

--- overAllProj/src/test/java/mainPack/Test1.java
+++ overAllProj/src/test/java/mainPack/Test1.java
@@ -7,7 +7,7 @@ public class Test1 {
     public void test1(){
         System.out.println("test 3");
         System.out.println("test 80");
-        System.out.println("test 90");
+        System.out.println("test 90k");
 
     }
 }
