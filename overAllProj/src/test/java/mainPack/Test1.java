6cf0ac39265b0577c09e76a3a5ce3df6bb33e7ac
Taras Radulenko <Taras_Radulenko@epam.com>
2016-10-26 15:37:55 +0300

    [QA] were added Driver and gitignore

--- overAllProj/src/test/java/mainPack/Test1.java
+++ overAllProj/src/test/java/mainPack/Test1.java
@@ -1,13 +1,35 @@
 package mainPack;
 
+import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Test1 {
+    WebDriver driver = new ChromeDriver();
+
     @Test
-    public void test1(){
+    public void test1() {
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://qalight.com.ua/");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("test 3");
         System.out.println("test 80");
         System.out.println("test 90k ");
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
 
     }
 }
