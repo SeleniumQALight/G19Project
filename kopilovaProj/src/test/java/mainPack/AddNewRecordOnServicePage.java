157aeb79c4d59fc721dc15686a349bfa5c7a2d19
Alina <aaa@aa.com>
2016-11-15 20:23:46 +0200

    Rejected commit: Default (4)

--- kopilovaProj/src/test/java/mainPack/AddNewRecordOnServicePage.java
+++ kopilovaProj/src/test/java/mainPack/AddNewRecordOnServicePage.java
@@ -3,9 +3,12 @@ package mainPack;
 
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.*;
+
 
 public class AddNewRecordOnServicePage {
     WebDriver driver = new FirefoxDriver();
@@ -13,7 +16,7 @@ public class AddNewRecordOnServicePage {
     HomePage homePage = new HomePage(driver);
     DictionaryServicePage dictionaryServicePage = new DictionaryServicePage(driver);
     ServiceEdit serviceEdit = new ServiceEdit(driver);
-
+    AllPages allPages = new AllPages(driver); // !!!!! dobavili
 
     @Test
     public void setUp() {
