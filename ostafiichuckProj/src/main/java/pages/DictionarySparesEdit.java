4569b2f26b0f64111084758266f29fa91dbcfefa
inviz <1>
2016-11-17 00:39:29 +0200

    [QA] new changes.

---
+++ ostafiichuckProj/src/main/java/pages/DictionarySparesEdit.java
@@ -0,0 +1,52 @@
+package pages;
+
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class DictionarySparesEdit {
+
+    WebDriver driver;
+    Logger logger;
+    final String errorInput = "Can not work with input UserName ";
+    final String errorButton = "Can not work with button ";
+
+    @FindBy(tagName = "select")
+    WebElement ddTypeOfSpares;
+
+
+    public DictionarySparesEdit(WebDriver exterDriver) {
+        this.driver = exterDriver;
+        logger = Logger.getLogger(getClass());
+        PageFactory.initElements(driver, this);
+    }
+
+    public void selectValueInDDTypeOfSpares(String textInDD) {
+        try {
+            Select select = new Select(ddTypeOfSpares);
+            select.selectByVisibleText(textInDD);
+            logger.info(textInDD + " was selected in DD");
+
+        } catch (Exception e) {
+            logger.error(errorInput + " DD TypeOfSpares");
+            Assert.fail(errorInput + " DD TypeOfSpares");
+        }
+    }
+
+    public void selectValueInDDTypeOfSparesByValue(String valueInDD) {
+        try {
+            Select select = new Select(ddTypeOfSpares);
+            select.selectByValue(valueInDD);
+            logger.info(valueInDD + " was selected in DD");
+
+        } catch (Exception e) {
+            logger.error(errorInput + " DD TypeOfSpares");
+            Assert.fail(errorInput + " DD TypeOfSpares");
+        }
+    }
+}
