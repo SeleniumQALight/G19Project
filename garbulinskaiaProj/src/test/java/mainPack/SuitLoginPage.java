034032970f2d77b0d9ebdb89df0094ca25bd0ca7
Lena <harbulinska@gmail.com>
2016-11-22 19:27:19 +0200

    new files

---
+++ garbulinskaiaProj/src/test/java/mainPack/SuitLoginPage.java
@@ -0,0 +1,17 @@
+package mainPack;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                InvalidLogOn.class,
+                ValidLogOn.class
+        }
+)
+
+public class SuitLoginPage {
+
+
+}
