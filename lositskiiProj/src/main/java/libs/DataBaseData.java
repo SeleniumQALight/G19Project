fa64b64f70ced46c944fb67c3018912205032328
lozik4 <lozik444@gmail.com>
2016-11-24 21:31:55 +0200

    work with DB

--- lositskiiProj/src/main/java/libs/DataBaseData.java
+++ lositskiiProj/src/main/java/libs/DataBaseData.java
@@ -12,11 +12,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DataBaseData {
 	static Logger log = Logger.getLogger(DataBaseData.class);
-	static  WebDriver driver;
+
 	static Database dB;
-	
+
+
 	public void dataBaseData (WebDriver driver) throws ClassNotFoundException, IOException, SQLException {
-		this.driver = driver;
+
 		
 	}
 	
@@ -27,6 +28,13 @@ public class DataBaseData {
 		String someValue1 = dB.selectValue("select HASH,CREATE_DATE from cs_check_portal where " + someData + "");
 		return someValue1;
 		//log.info("Данные выбраны");
+	}
+
+	public static String getPassForLogin(String login) throws SQLException, IOException, ClassNotFoundException {
+		dB = new Database("MySQL_PADB_DB", "MySQL");
+		String pass = dB.selectValue("select pass from table_selenium where login='"+login+"'");
+		dB.quit();
+		return pass;
 	}
 	
 }
