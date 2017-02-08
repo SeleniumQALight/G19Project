a5b8cdf113a61accc98e95773ed84f3549fd23e1
Taras Radulenko <Taras_Radulenko@epam.com>
2017-02-08 15:49:00 +0200

    test push to dev

--- overAllProj/src/test/java/mainPack/InvalidLogOn.java
+++ overAllProj/src/test/java/mainPack/InvalidLogOn.java
@@ -15,7 +15,7 @@ public class InvalidLogOn {
     @Test
     public void setUp(){
         loginPage.openBrowserAndLoginPage();
-        loginPage.enterUserName("Student2");
+        loginPage.enterUserName("Student3");
         loginPage.enterPassWord("906090");
         loginPage.clickButtonVhod();
         Assert.assertTrue(loginPage.isFormLoginPresent());
